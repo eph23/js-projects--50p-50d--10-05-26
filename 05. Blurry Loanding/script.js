83f5072b40e23f7446405255f6aaf25bda5e4abd
Eph23 <ephraims.818@gmail.com>
2026-05-12 10:27:09 +0600

    JS-PROJECTS--50P50D -- Blurry Loading - 02. Load Blur Effect

--- 05. Blurry Loanding/script.js
+++ 05. Blurry Loanding/script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const loadText = document.querySelector(".loading-text");
+const bg = document.querySelector(".bg");
+
+let load = 0;
+
+const scale = function (number, inMin, inMax, outMin, outMax) {
+    return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+};
+
+const blurring = function () {
+    load++;
+
+    if (load > 99) {
+        clearInterval(int);
+    }
+
+    loadText.innerText = `${load}%`;
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+};
+
+let int = setInterval(blurring, 30);
