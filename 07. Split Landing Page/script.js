323f9b08050f3c3afad862bed8cd6739df1d6856
Eph23 <ephraims.818@gmail.com>
2026-05-13 19:21:57 +0600

    JS-PROJECTS--50P50D -- Split Landing Page - 02. The Hover Effect

--- 07. Split Landing Page/script.js
+++ 07. Split Landing Page/script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const container = document.querySelector(".container");
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const text = document.querySelector(".text-container");
+
+left.addEventListener("mouseenter", () => {
+    container.classList.add("hover-left");
+});
+left.addEventListener("mouseleave", () => {
+    container.classList.remove("hover-left");
+});
+
+right.addEventListener("mouseenter", () => {
+    container.classList.add("hover-right");
+});
+right.addEventListener("mouseleave", () => {
+    container.classList.remove("hover-right");
+});
